911fb824d1c56530d2ef9f76a32d14c52784dad2
color <95304946+HermanMazur@users.noreply.github.com>
2022-12-03 01:13:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ async function invokeAction({ action, id, name, email, phone }) {
             const contactGetById = await contactsOperation.getContactById(id);
             console.log(contactGetById);
             break;
+        
+        case "add":
+            const newContact = await contactsOperation.addContact(name, email, phone);
+            console.log(newContact);
+            break;
+        
+        case "remove":
+            const removeContact = await contactsOperation.removeContact(id);
+            console.log(removeContact);
+            break;
+        
+        default:
+            console.log("\x1B[31m Unknown action type!");
     }
 }
 
